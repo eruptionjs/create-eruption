a28e5ec0ecba5ba6ce1c59a97bbc7706980fd888
Raí Siqueira <me@raisiqueira.io>
2023-02-15 01:57:30 +0000

    chore: base project

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,1 +1,23 @@
-export const lib = () => 'lib';
+import { intro, outro, text, isCancel, cancel } from '@clack/prompts';
+import kleur from 'kleur';
+
+export async function main() {
+  intro(`Welcome to Eruption CLI 🌋`);
+
+  const projectName = await text({
+    message: 'What is the name of your project?',
+    initialValue: 'eruption',
+    placeholder: 'E.g: my-awesome-project',
+    validate(value) {
+      if (value.length === 0) {
+        return `The project name is required!`;
+      }
+    },
+  });
+
+  if (isCancel(projectName)) {
+    cancel('Operation cancelled 💔');
+  }
+
+  outro('Well done!');
+}
