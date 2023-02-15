aafe3c2f2d96981dfeb04ddd0a1da6e37faf05b0
Raí Siqueira <me@raisiqueira.io>
2023-02-15 23:50:49 +0000

    feat: clone the project from github
    Signed-off-by: Raí Siqueira <me@raisiqueira.io>

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,8 +1,12 @@
-import { intro, outro, text, isCancel, cancel } from '@clack/prompts';
-import kleur from 'kleur';
+import { intro, outro, text, select, confirm, spinner, note } from '@clack/prompts';
+import { underline, green } from 'kleur';
+import { setTimeout } from 'node:timers/promises';
+
+import { AVAILABLE_KITS } from './constants.mjs';
+import { handleCancelation, getKitFromGitHub } from './utils';
 
 export async function main() {
-  intro(`Welcome to Eruption CLI 🌋`);
+  intro(`Welcome to the Eruption CLI 🌋`);
 
   const projectName = await text({
     message: 'What is the name of your project?',
@@ -15,9 +19,47 @@ export async function main() {
     },
   });
 
-  if (isCancel(projectName)) {
-    cancel('Operation cancelled 💔');
+  handleCancelation(projectName);
+
+  const kit = await select({
+    message: 'Select your Eruption kit',
+    options: AVAILABLE_KITS,
+  });
+
+  handleCancelation(kit);
+
+  const initGitRepo = await confirm({
+    message: 'Do you want initialize a git repo?',
+    initialValue: false,
+  });
+
+  handleCancelation(initGitRepo);
+
+  const install = await confirm({
+    message: 'Want continue?',
+    initialValue: false,
+  });
+
+  handleCancelation(install);
+
+  if (install) {
+    const s = spinner();
+    s.start('Starting the project ⏳');
+    await getKitFromGitHub(kit as string, projectName as string);
+    s.stop('Done ✅');
   }
 
-  outro('Well done!');
+  const nextSteps = `cd ${projectName as string}        \n${
+    install ? '' : 'npm install\n'
+  }npm run dev`;
+
+  note(nextSteps, 'Next steps:');
+
+  await setTimeout(1000);
+
+  outro(
+    `Well done! If you have any problem, feel free to open an issue: ${underline(
+      green('https://github.com/eruptionjs/create-eruption')
+    )}`
+  );
 }
---
+++ lib/src/utils.ts
@@ -0,0 +1,70 @@
+import { isCancel, cancel } from '@clack/prompts';
+import { exec } from 'child_process';
+import fs from 'fs/promises';
+import path from 'path';
+import tiged from 'tiged';
+
+/**
+ * Handle the user cancelation in the CLI.
+ * @param value The value of Symbol of the clack prompt.
+ */
+export function handleCancelation(value: unknown) {
+  if (!value) return;
+  if (isCancel(value)) {
+    cancel('Operation cancelled 💔');
+    process.exit(0);
+  }
+}
+
+/**
+ * Check if a file exists.
+ */
+export async function fileExists(path: string) {
+  try {
+    await fs.stat(path);
+    return true;
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      return false;
+    } else {
+      throw error;
+    }
+  }
+}
+
+/**
+ * Exec a command to initialize a new git repository.
+ */
+export async function initGitRepo(path: string) {
+  return new Promise((resolve, reject) => {
+    exec(`git init ${path}`, (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(undefined);
+      }
+    });
+  });
+}
+
+export async function getKitFromGitHub(kit: string, projectName: string) {
+  // TODO: Implement using tiged;
+  /** example: eruptionjs/core - That means tiged will clone the core repository. */
+  const repoName = `eruptionjs/${kit}`;
+  /** The destiny folder where the project will be cloned. */
+  const destFolder = path.join(process.cwd(), projectName);
+
+  const emitter = tiged(repoName, {
+    cache: false,
+    force: true,
+    verbose: false,
+    mode: 'tar',
+  });
+
+  try {
+    await emitter.clone(destFolder);
+  } catch (error: unknown) {
+    cancel(error instanceof Error ? error.message : 'Something wrong happens 💔');
+    process.exit(1);
+  }
+}
